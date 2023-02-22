0ace9966cb24e6fadf4dbbd8870889d5f56daab4
Derek Leadbetter <dleadbetter55@gmail.com>
2023-02-22 14:42:03 -0500

    UDCSL #175 - Adding a rake task to pull in IIIF Resources from another application

--- lib/triple_eye_effable/engine.rb
+++ lib/triple_eye_effable/engine.rb
@@ -1,5 +1,9 @@
 module TripleEyeEffable
   class Engine < ::Rails::Engine
     isolate_namespace TripleEyeEffable
+
+    config.before_initialize do
+      config.i18n.load_path += Dir["#{config.root}/config/locales/**/*.yml"]
+    end
   end
 end
